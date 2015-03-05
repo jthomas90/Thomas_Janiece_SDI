579da24ba9edc617c9dbb1d78d143fb56978017e
jthomas90 <jthomas1@gmail.com>
2015-03-05 16:04:49 -0500

    Math Class
    Talked about =,-,*,/ and Modulo

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -44,5 +44,53 @@ a=a+3;
 console.log(a);
 
 
+//Declare new variable
+var b;
+
+//Define this variable using a existing variable
+
+b = a+3;
+console.log(a);
+console.log(b);
+
+//Simple Math
+//Find our ages
+
+//Declare and define the year you were born
+var yearBorn = 1990;
+
+//2015 - year born
+var ourAge = 2015 - yearBorn;
+console.log(ourAge);
+
+//Talk about Math
+// +, -, *, /
+
+//Find the area of triangle
+//base* height/2
+
+var base = 8;
+var height = 5;
+
+var areaTriangle = base*height/2;
+console.log(areaTriangle);
+
+// Modulo - %
+// Gives the remainder
+
+var decimal = 32/10;
+console.log(decimal);
+
+var remainder = 32%10;
+console.log(remainder);
+
+//Find out if even or odd
+// %2 1=odd 2= even
+var evenOrOdd = 67%2;
+console.log(evenOrOdd);
+
+
+
+
 
 
