510b2c7b9f88c87106c675085468ba140956b4d3
jthomas90 <jthomas1@gmail.com>
2015-03-05 14:54:09 -0500

    Day 2 Into Files
    Created HTML and js file for Day 2

---
+++ Day 2/js/script.js
@@ -0,0 +1,20 @@
+/*
+Janiece Thomas
+3/05/2015
+Section 00
+Day 2 Intro
+ */
+
+//alert("Testing to see if connected");
+
+/*
+Example of a Multi-lined comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+//Single lined comment. ONLY this line will be ignored.
+
+//alert boxes- pop up box that will alert the user with information.
+
+alert("Text that the user will see.");
+
