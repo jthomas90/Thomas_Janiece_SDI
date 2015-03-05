b2b317ad212d75f9f69b5ea2b71cb7459295263f
jthomas90 <jthomas1@gmail.com>
2015-03-05 16:56:23 -0500

    Strings, Boolean, and PEMDAS
    Strings, Boolean, and PEMDAS OH MY!

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -36,9 +36,7 @@ console.log(whatever1);
 
 //Declare and define a variable at the same time
 var a = 2;
-
 console.log(a);
-
 a+3;
 a=a+3;
 console.log(a);
@@ -80,7 +78,6 @@ console.log(areaTriangle);
 
 var decimal = 32/10;
 console.log(decimal);
-
 var remainder = 32%10;
 console.log(remainder);
 
@@ -88,6 +85,83 @@ console.log(remainder);
 // %2 1=odd 2= even
 var evenOrOdd = 67%2;
 console.log(evenOrOdd);
+
+//Assignment Operators
+/*
+= Assigns the value to the variable
+++ Adds 1 to the current value of the variable
+-- Subtract 1 from the current value of the variable
++= # Adds the number to the current value of the variable
+-= # Subtracts the number from the current value of the variable
+/= Divide the current value by the number
+*= # Multiply the current value by the number
+ */
+ var assign = 0;
+
+// ++ is the same as assign =assign+1;
+assign++;
+console.log(assign);
+
+// -- is the same as assign = assign-1;
+assign--;
+console.log(assign);
+
+// += # is the same as assign = assign+#;
+assign+=5; //assign = assign+5
+console.log(assign);
+
+// -= # is the same as assign = assign-#;
+assign=assign-2;
+console.log(assign);
+
+// /= is the same as assign= assign/#
+assign/=3;
+console.log(assign);
+
+// *= is the same as assign=assign*#
+assign*=7;
+console.log(assign);
+
+//Strings - amy text that we use
+//Quotes are needed to distinguish between variables and plain text\
+
+var kermit = "light green";
+var frogName = "kermit";
+console.log(frogName);
+
+// To double quote or not to double quote
+// Can use an escape character \ backslash in front of the '
+var phrase = 'I don\'t know';
+console.log(phrase);
+
+// Escape character can also do multiple lines \n - new line character
+var phrase2 = "I don't know. \nYou never do!";
+console.log(phrase2);
+
+// Boolean - kind of like a light switch
+// Either true of false
+// Not a text string and must be lower case!!
+var yes = true;
+
+var nope = false;
+
+//Order of Operation
+// PEMDAS - Please Excuse My Dear Aunt Sally
+// Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
+
+// Find the Avereage quiz grades
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 90;
+
+// Average - Add them up and divide by the numbers of quizes
+var average = (quiz1 + quiz2 + quiz3)/3;
+console.log(average);
+
+
+
+
 
 
 
