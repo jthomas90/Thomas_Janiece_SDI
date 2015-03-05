5e6de7c5edf8868ec2cb36af31090339c79b5ef9
jthomas90 <jthomas1@gmail.com>
2015-03-05 18:23:23 -0500

    Day 2 Lab Expressions
    Slices of pizza part 1

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -183,7 +183,8 @@ var num = 5.5678912345;
 //Round to 2 decimal places
 var n = num.toFixed(2);
 
-console.log(n);
+console.log("The rounded number to 2 decimal places is "+n);
+
 
 
 
---
+++ Day2LabExpressions/js/script.js
@@ -0,0 +1,25 @@
+/*
+ Janiece Thomas
+ 3/05/2015
+ Section 00
+ Day 2 Lab Expressions
+ */
+
+//alert("hey");
+
+
+// Slice of Pie Part 1
+//Calculate How much pizza each person eats
+var slices = 12;
+var people = 25;
+var pizza  = 7;
+
+//Multiply pizza by slices then divide by people
+
+var TPA = (pizza*slices)/people;
+
+//outcome
+console.log("The number of slices each partier will get is " + TPA +" slices.");
+
+
+
