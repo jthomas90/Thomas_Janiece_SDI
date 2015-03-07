6306a8f7c978c8461f23d741050bf93fbb99727a
jthomas90 <jthomas1@gmail.com>
2015-03-07 18:53:38 -0500

    Calculations of variables
    Variables set up to calculate equation

--- Thomas_Janiece_Expressions_Assignment/js/Day3assignment.js
+++ Thomas_Janiece_Expressions_Assignment/js/Day3assignment.js
@@ -7,7 +7,7 @@ Day 3 Assignment
  */
 
 
-//alert("Just testing!");
+alert("This is program is to help you find out how much you get paid before taxes are taken out First.\nThen about how much you'll actually get with your approximate tax input.");
 
 
 
@@ -16,9 +16,21 @@ Day 3 Assignment
  myArray[0] = prompt("How many hours a day do you work a day?\n1-24 Hours.");
  myArray[1] = prompt("How much do you get paid hourly?");
  myArray[2] = prompt("How many days a week do you work?");
-
+ myArray[3] = prompt("How much tax is typically taken out per week?");
 
 console.log(myArray);
+
+ //Sub Variables
+ hoursWorked = myArray[0];
+ workPay = myArray[1];
+ daysWorked = myArray[2];
+
+//Calculate hours*pay*days
+var total = myArray[0] * myArray[1];
+var fullTotal = total *=myArray[2];
+ var withTax = fullTotal - myArray[3];
+
+
 
 
 
