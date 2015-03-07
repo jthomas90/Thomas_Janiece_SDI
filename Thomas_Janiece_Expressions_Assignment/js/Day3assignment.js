706e7e85ccfaaa1c3a1214036dbea7dca6d1f32f
jthomas90 <jthomas1@gmail.com>
2015-03-07 18:21:48 -0500

    Variables Created
    First set of variables created

--- Thomas_Janiece_Expressions_Assignment/js/Day3assignment.js
+++ Thomas_Janiece_Expressions_Assignment/js/Day3assignment.js
@@ -9,4 +9,11 @@ Day 3 Assignment
 
 //alert("Just testing!");
 
+
+
+//Create Variables
+var hoursWorked = prompt("How many hours a day do you work a day?\n1-24 Hours."),
+    hourlyPay = prompt("How much do you get paid hourly?"),
+    daysWorked = prompt("How many days a week do you work?");
+
  
