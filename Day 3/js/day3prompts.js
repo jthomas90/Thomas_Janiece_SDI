516ec34d909b67245bb3cc298c572e5295063ec3
jthomas90 <jthomas1@gmail.com>
2015-03-07 15:27:51 -0500

    Learned to prompt
    Be specific and make the results matter

---
+++ Day 3/js/day3prompts.js
@@ -0,0 +1,33 @@
+/*
+Janiece Thomas
+Section 00
+3/7/2015
+Day 3 Prompts
+ */
+
+
+//alert("Just testing!");
+
+//Ask teg user for input
+//prompt("Text that the user will see)
+
+//prompt("Enter year of birth");
+
+var userInput = prompt("Enter year of birth:");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+//Explain to the user what is going on
+var height = prompt("Let's calculate the area of the rectangle!\nPlease enter the height:");
+
+var width = prompt("Please enter the width of the rectangle:");
+
+//Calculate the area of the rectangle
+var area = width*height;
+
+//Tell te user the answer
+console.log("The area of the rectangle with a height of "+height+ " and a width of "+ width+ " is " +area + ".");
+
+var results = "The results of the area is "+ area+ ".";
+
+console.log(results);
