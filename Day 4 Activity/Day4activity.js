7ddb44d12685913c8d2a620a22f9fdbf45f7216f
jthomas90 <jthomas1@gmail.com>
2015-03-10 18:46:31 -0400

    Start of Activity Day 4 Celsius and Fahrenheit
    Start of Activity Day 4 Celsius and Fahrenheit

---
+++ Day 4 Activity/Day4activity.js
@@ -0,0 +1,18 @@
+
+ /*
+Janiece Thomas
+Section 00
+3/10/15
+Day 4 Activity
+ */
+
+
+//alert("Just testing!");
+
+ //Celsius to Fahrenheit converter
+
+ //Variable for prompt to decide which way to covert
+
+ var q1 = prompt("What is the temperature?");
+var  q2 = prompt("Is that in Celsius or Fahrenheit?\nC or F ");
+ 
