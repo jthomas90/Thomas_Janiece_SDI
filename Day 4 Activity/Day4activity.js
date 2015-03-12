e7d9f2d315c1a302d7e87429d40b6c708d478f4c
jthomas90 <jthomas1@gmail.com>
2015-03-12 13:46:52 -0400

    Day5 Steaks

--- Day 4 Activity/Day4activity.js
+++ Day 4 Activity/Day4activity.js
@@ -53,9 +53,15 @@ if (username === cUsername && (pass === cPass) ){
 var age = prompt("How old are you?");
  var movieTime = prompt("What time doe the movie start?\n1-12");
 
-
-// if(age =< 10 || age <55 ){
- //    console.log("Your ticket is $7.00");
- //}
+//If the person is younger then 10 or older the 55 their ticket is $7
+ if(age <= 1 || age <= 55 ){
+     console.log("Your ticket is $7.00");
+     //If the person is watching the movie before 3 but after 5 they also get a discount.
+ } else if(movieTime <= 3 && movieTime >= 5){
+     console.log("Your ticket is $7.00");
+ } else {
+     //The person does not get discount
+     console.log("Your ticket is $12.00");
+ }
 
 
---
+++ Day 5/js/Day5steaks.js
@@ -0,0 +1,10 @@
+
+ /*
+Janiece Thomas
+Section 00
+3/12/15
+Day 5 Steaks
+ */
+
+
+//alert("Just testing!");
