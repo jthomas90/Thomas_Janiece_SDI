a81e29bf2ecf2f86f55eedd6c22dd33992b9a5d2
jthomas90 <jthomas1@gmail.com>
2015-03-12 14:35:16 -0400

    Finished Day 4 Activity
    Had to change <>

--- Day 4 Activity/Day4activity.js
+++ Day 4 Activity/Day4activity.js
@@ -54,10 +54,10 @@ var age = prompt("How old are you?");
  var movieTime = prompt("What time doe the movie start?\n1-12");
 
 //If the person is younger then 10 or older the 55 their ticket is $7
- if(age <= 10 || age <= 55 ){
-     console.log("Your ticket is $7.00");
+ if(age <= 10 || age >= 55 ){
+     console.log("Your ticket is $7.00.");
      //If the person is watching the movie before 3 but after 5 they also get a discount.
- } else if(movieTime <= 3 && movieTime >= 5){
+ } else if(movieTime >= 3 && movieTime <= 5){
      console.log("Your ticket is $7.00");
  } else {
      //The person does not get discount
