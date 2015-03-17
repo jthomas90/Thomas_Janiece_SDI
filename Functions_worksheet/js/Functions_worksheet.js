459014c01c47f07e77d88f7e08660894b5117712
jthomas90 <jthomas1@gmail.com>
2015-03-17 18:55:33 -0400

    Calculate the circumference of a Circle
    DONE

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -6,9 +6,25 @@ Section 00
  Functions_worksheet
  */
 
-
+var radius = 1;
 //alert("Just testing!");
 
  //Calculate the circumference of a Circle
  //Create a function
 //FORMULA FOR RADIUS - #/3.14
+
+function circleCir(r){
+     //calculate radius
+     var cirCircum = 2*r*Math.PI;
+     console.log("Inside the function the circumference is "+cirCircum+".");
+     return cirCircum;
+ }
+
+ //call function
+ circleCir(10);
+ //Create a variable for the return value
+ var results = circleCir(radius);
+
+
+//Results
+ console.log("The circumference of the circle is "+results.toFixed(2)+".");
