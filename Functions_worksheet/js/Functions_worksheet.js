b1ffa0f035481eb46b77fd67e437a213dcc65752
jthomas90 <jthomas1@gmail.com>
2015-03-17 19:16:28 -0400

    Stung 3rd commit

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -28,3 +28,24 @@ function circleCir(r){
 
 //Results
  console.log("The circumference of the circle is "+results.toFixed(2)+".");
+
+
+
+//STUNG
+ //Calculate a function
+
+ function stung(w,b){
+     var b = 8.666666667;
+      var howMany = w*b;
+     //Console.log
+     console.log("It takes "+ howMany + " bee stings to kill this animal")
+ }
+ //Prompt the user for their weight
+ var weight = prompt("How many pounds do yo weigh?\nNumbers only please");
+ if(weight === "" || isNaN(weight) ){
+     //This code will run if the variable is blank or not a number
+     //Re prompt user
+     weight = prompt("Please do not leave blank and only use numbers.\nWhat is you weight?")
+ }
+
+ 
