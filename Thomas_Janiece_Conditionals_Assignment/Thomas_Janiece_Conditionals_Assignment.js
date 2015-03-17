fad7f4eb4fde2947182dfc5f23a7d390aaf17f67
jthomas90 <jthomas1@gmail.com>
2015-03-17 13:35:37 -0400

    Day 7 Beer
    Starting Day 7

--- Thomas_Janiece_Conditionals_Assignment/Thomas_Janiece_Conditionals_Assignment.js
+++ Thomas_Janiece_Conditionals_Assignment/Thomas_Janiece_Conditionals_Assignment.js
@@ -9,6 +9,7 @@ Conditionals_Assignment
 
 //alert("Just testing!");
 
- 
+//Find out if the user wants a roommate
+ var roommy = prompt("This calculater is to help you determine will you need a roomate or not.\nDo you want a roommate?");
 
 
