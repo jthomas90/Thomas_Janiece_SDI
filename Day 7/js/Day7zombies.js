a0b848ab584fc62406779e2318121db1522f71af
jthomas90 <jthomas1@gmail.com>
2015-03-17 14:16:51 -0400

    Zombie horde finished
    Zombie horde finished

--- Day 7/js/Day7zombies.js
+++ Day 7/js/Day7zombies.js
@@ -26,6 +26,7 @@ Day 7 Zombies
  //Number of days that the CDC wants
  var days = 8;
 
+ /*
  //Create for loop to cal zombie number
  for(var i = 1; i<=days; i++){
 
@@ -41,7 +42,24 @@ Day 7 Zombies
  }
 
 
+*/
+
 //How long will it take to get million zombies
 
-
+var numDays = 1;
  
+ while(numZombies <=1000000){
+     //How many NEW get made everyday
+     var newZombies = numZombies * numBites;
+
+     //Add New Zombie to existing horde
+     numZombies += newZombies ;
+
+     //Console.log the results
+     console.log("Their are "+ numZombies+ " numbers of zombies on day #"+numDays+"!");
+
+     numDays++;
+
+ }
+
+ console.log("It will take " +(numDays-1)+" days to reach a million zombies.");
