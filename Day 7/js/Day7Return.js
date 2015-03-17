04d9dad65e17efb8bf18cd9581e1329da8d1bfb4
jthomas90 <jthomas1@gmail.com>
2015-03-17 16:52:22 -0400

    Day 7 Return Finished
    Day 7 Return Finished

--- Day 7/js/Day7Return.js
+++ Day 7/js/Day7Return.js
@@ -8,3 +8,40 @@ Section 00
 
 
 //alert("Just testing!");
+
+ //Return a value to our main code
+
+ //Create a function to calculate the are of a rectangle
+ function calcArea(w,h){
+     //Calculate area
+     var area = w*h;
+     console.log("Inside the function the ares is "+area+".");
+     //Return the are to the main code
+     return area;
+ }
+
+ //call function
+ //Create a variable to catch the return value
+ var rectArea = calcArea(10,20);
+
+ //console.log the area
+ console.log(rectArea);
+
+
+
+ //Create a function to calculate the area of a circle
+ function circleArea(r){
+     //calculate area  Pi * r * r
+     var area = Math.PI *r *r;
+     //return the value
+     return area;
+ }
+
+ //Function call this circleArea
+ //Create a variable to catch teh returned area
+ var circArea = circleArea(6);
+ console.log("The are of the circle is "+circArea+".");
+
+ //Whats is twice the circle area of a 6' circle
+ var resultsTwice= circArea*2;
+ console.log("Twice the are is "+resultsTwice+".");
