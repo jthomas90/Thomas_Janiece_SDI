5bb13c94ad1f5e1aaf47e1e39ad28d2dfd5c595c
jthomas90 <jthomas1@gmail.com>
2015-03-17 13:52:47 -0400

    End of 99 bottles of beer FOR LOOP
    For loop

--- Day 7/js/Day7beer.js
+++ Day 7/js/Day7beer.js
@@ -3,8 +3,24 @@
 Janiece Thomas
 Section 00
 3/17/15
-Day 3 Prompts
+Day 7 Beer
  */
 
 
 //alert("Just testing!");
+
+ // Code the song 99 Bottles of beer on the wall.
+
+ //basic structure of FOR loop
+ // for(var 1 = 0; condition to test ;increment of change) {}
+
+ for(var i = 99; i>0;i--)  {
+     if (i===1){
+         console.log("1 bottle of beer on the wall. 1 bottle of beer. You take one down and pass it around. No more bottles of beer on the wall.\nThe End.");
+
+     } else {
+         console.log(i+" bottles of beer on the wall. "+i+ " bottles of beer. You take one down and pass it around. "+(i-1)+" bottles of beer on the wall.");
+     }
+
+
+ }
