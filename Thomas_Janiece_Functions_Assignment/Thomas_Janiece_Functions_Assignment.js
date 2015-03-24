9335bcbcf10e146aa621a8c28cdfbded27b3b1aa
jthomas90 <jthomas1@gmail.com>
2015-03-24 18:43:42 -0400

    Created a while loop
    Created a while loop

--- Thomas_Janiece_Functions_Assignment/Thomas_Janiece_Functions_Assignment.js
+++ Thomas_Janiece_Functions_Assignment/Thomas_Janiece_Functions_Assignment.js
@@ -8,3 +8,23 @@ Thomas Janiece Functions Assignment
 
 
 //alert("Just testing!");
+
+
+//Create prompt to find out if they are male or female
+ var gender = prompt("This is a calculator to find out what you total body fat is.\nAre you a male of female?");
+
+ //Validate gender value
+ while (gender==="" || ! isNaN(gender) || gender!="Female" ||gender!="female" || gender!="Male" ||gender!="male" ){
+     if(gender===""){
+     //re prompt user
+         gender = prompt("Please do not leave blank and only use letters.\nAre you male or female?");
+     } else if (!isNaN(gender)){
+         //re prompt user
+         gender = prompt("Please do not enter any numbers.\nAre you male or female?");
+     } else if (gender!="Female" ||gender!="female" || gender!="Male" ||gender!="male"){
+         //re prompt user
+         gender = prompt("Please enter male or female?")
+     }
+
+ }
+
