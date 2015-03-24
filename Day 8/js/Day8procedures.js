0d79c8c758d3acd966874c5c9ecb36816ac64b59
jthomas90 <jthomas1@gmail.com>
2015-03-24 15:09:37 -0400

    Day 8 Procedures
    Doesn’t return a value

--- Day 8/js/Day8procedures.js
+++ Day 8/js/Day8procedures.js
@@ -8,3 +8,28 @@ Day 8 Procedures
 
 
 //alert("Just testing!");
+
+ //This is a basic function
+ function calcAreaF (w,h){
+     var area = w*h;
+     return area;
+ }
+
+ //Function call
+ var area = calcAreaF(6,8);
+ console.log("The area of the rectangle is "+ area+".");
+
+ //This is a procedure
+ //A list of steps to do
+
+ function calcAreaP(w,h){
+     var area = w*h;
+
+     //we do not return this value
+     console.log("Inside the procedure the area is "+area+".");
+
+ }
+
+ //Function call for the procedure
+ //Just the function name NO VARIABLE TO CATCH
+ calcAreaP(12,16);
